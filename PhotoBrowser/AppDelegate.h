4243067ffa2be7f7338c463c76f1aabaf673298d
ssrinivasan <sowmya.srinivasan@wipro.com>
2018-05-07 09:48:40 +0530

    Initial Commit

---
+++ PhotoBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhotoBrowser
+//
+//  Created by Sowmya Srinivasan on 07/05/18.
+//  Copyright © 2018 Wipro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PhotoBrowser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhotoBrowser
+//
+//  Created by Sowmya Srinivasan on 07/05/18.
+//  Copyright © 2018 Wipro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
